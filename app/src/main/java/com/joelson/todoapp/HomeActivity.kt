da94ddd754ca84a37e30c98fa5c817b73436d6f1
Joelxon <isaacjoelson@gmail.com>
2021-06-25 11:59:49 -0700

    commit profile

--- app/src/main/java/com/joelson/todoapp/HomeActivity.kt
+++ app/src/main/java/com/joelson/todoapp/HomeActivity.kt
@@ -1,19 +1,32 @@
 package com.joelson.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.joelson.todoapp.activities.ProfileActivity
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var userNameDisplay: TextView
+    private lateinit var profileButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         userNameDisplay = findViewById(R.id.userNameDisplay)
+        profileButton = findViewById(R.id.profileButton)
 
         userNameDisplay.text = "welcome, ${intent.getStringExtra("")}"
+        profileButton.setOnClickListener {
+            openProfilePage()
+        }
+    }
+
+    private fun openProfilePage (){
+        val myIntent = Intent (this, ProfileActivity::class.java)
+        startActivity(myIntent)
     }
 }
